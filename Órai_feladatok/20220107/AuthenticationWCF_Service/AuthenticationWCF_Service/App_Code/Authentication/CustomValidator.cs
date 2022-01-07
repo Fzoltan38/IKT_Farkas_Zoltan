bbd0bb55536dd314a0c61e7749ae86ba27c42d4d
Gabriella <hz.gabriella96@gmail.com>
2022-01-07 12:51:02 +0100

    Username Password authentication

---
+++ Órai_feladatok/20220107/AuthenticationWCF_Service/AuthenticationWCF_Service/App_Code/Authentication/CustomValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web;
+using AuthenticationWCF_Service.Models;
+
+namespace AuthenticationWCF_Service.App_Code.Authentication
+{
+    public class CustomValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            AccountModel am = new AccountModel();
+            if (am.login(userName, password))
+                return;
+            throw new SecurityTokenException("Nincs regisztrálva!");
+        }
+    }
+}
---
+++ Órai_feladatok/20220107/AuthenticationWCF_Service/AuthenticationWCF_Service/Models/AccountModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationWCF_Service.Models
+{
+    public class AccountModel
+    {
+        private List<Account> listAccount = new List<Account>();
+
+        public AccountModel()
+        {
+            listAccount.Add(new Account { Username = "acc1", Password = "123" }) ;
+            listAccount.Add(new Account { Username = "acc2", Password = "123" });
+            listAccount.Add(new Account { Username = "acc3", Password = "123" });
+        }
+
+        public bool login(string username, string password)
+        {
+            return listAccount.Count(acc => acc.Username.Equals(username) &&
+            acc.Password.Equals(password)) > 0;
+        }
+    }
+}
